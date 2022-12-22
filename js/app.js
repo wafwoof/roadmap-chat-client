9dc118ebb0b1ad478f8e4adfe0f5467e556c512d
unknown <kazmcquaid@gmail.com>
2022-12-21 19:40:26 -0800

    Commit 1

--- js/app.js
+++ js/app.js
@@ -10,39 +10,43 @@ window.addEventListener('DOMContentLoaded', (event) => {
   // hard-coded username string
   var username = "Administrator";
 
+  async function printNewestMessage() {
+    let response = await fetch('http://139.177.195.118:8801/chat/log');
+    let responseJSON = await response.json();
+
+    // index[0] should be the first message in the log
+    let index = 0;
+
+    console.log("--Generating New Message!");
+
+    console.log(responseJSON.messages[index]);
+    const messageTemplateHTML = `
+      <div class="message">
+        <p class="messageUsername">
+          ${responseJSON.messages[index].username}
+        </p>
+        <p class="messageContent">${responseJSON.messages[index].content}</p>
+      </div>
+    `;
+
+    const messageContainer = document.getElementById("message-container");
+    const newMessage = document.createElement("div");
+    newMessage.innerHTML = messageTemplateHTML;
+    messageContainer.appendChild(newMessage);
+
+    console.log("--Finished Generating New Message!");
+  }
+
   // Hookup Send Button
   function clickHandler() {
     var message = chatboxInput.value;
     console.log(username, "says:", message);
-
+  
+    // SEND MESSAGE TO SERVER HERE chat/submit
     // SEND MESSAGE TO SERVER HERE chat/submit
   }      
-
-  async function getMessageLog() {
-    let response = await fetch('http://139.177.195.118:8801/chat/log');
-    let responseJSON = await response.json()
-    return responseJSON;
-  }
-
-  function generateMessage(data, index) {
-    const messageTemplateHTML = `
-      <div class="message">
-        <p class="messageUsername">
-          ${data.messages[index].username}
-        </p>
-        <p class="messageContent">${data.messages[index].username}</p>
-      </div>
-    `;
-
-    const newMessage = document.createElement("div");
-    newMessage.innerHTML = messageTemplateHTML;
-    console.log("Finished Generating a Message!")
-  }
-      
-  getMessageLog()
-    .then(console.log("Test Message Log (index 0):"))
-    .then(data => console.log(data.messages[0]))
-    .then(data => generateMessage(data, 0));
-
+  
+  
+  printNewestMessage();
   
 })
